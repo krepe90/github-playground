94fbdb0096468be86f6ff5780161390f57845fc1
krepe90 <krepe90@gmail.com>
2023-05-28 15:18:20 +0900

    ..

--- src/test2.py
+++ src/test2.py
@@ -7,4 +7,26 @@ def test():
         print(file_name)
     some_right_name = 2.13
     somg_strange_name = 123
+    long_list = [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+    ]
     return
